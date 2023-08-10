7bd35107a69d8f942d3aa8d35da67904826f8b90
Kyle Van Houtan <kyle.vanhoutan@gmail.com>
2023-08-10 16:54:33 -0400

    fixing gradients

--- script/6_morphs.R
+++ script/6_morphs.R
@@ -51,7 +51,7 @@ Spectral13 <- getPalette(colourCount) # 13 color Spectral palette
 # then make the plot and call in the expanded Brewer pal
 ggplot(morph_HWi, aes(x = MEASURE, fill = CODE)) + 
   themeKV + theme(legend.position = "none") +
-  scale_fill_manual(values = getPalette(colourCount)) +
+  scale_fill_manual(values = Spectral13) +
   #  scale_fill_brewer(palette = "Spectral") +
   theme(axis.text.y = element_blank(),
         axis.title.y = element_blank(),
@@ -81,14 +81,20 @@ p1 <- ggplot(morph_HWi, aes(x = MEASURE, y = fct_reorder(CODE,MEASURE), fill = a
                   axis.title.x = element_text(size = 8),
                   axis.text.y = element_text(size = 7),
                   axis.title.y = element_text(size = 8),) +
-  scale_fill_gradientn(colours = c("#9e0142", "#d53e4f",  "#fdae61", "#fee08b", "#e6f598", "#abdda4", "#66c2a5", "#3288bd", "#5e4fa2")) +
+  geom_density_ridges_gradient(scale = 2.5, alpha = 0.2, size = 0.2, rel_min_height = 0.01, bandwidth = 1) +
+  scale_fill_gradientn(colours = c("#9e0142", "#d53e4f",  "#fdae61", "#fee08b", "#e6f598", "#abdda4", "#66c2a5", "#3288bd", "#313695")) +
   # more detail at https://ggplot2.tidyverse.org/reference/scale_gradient.html
-  geom_density_ridges_gradient(scale = 2.5, alpha = 0.6, size = 0.2, rel_min_height = 0.01, bandwidth = 1) +
+  # also here https://r-graphics.org/recipe-colors-palette-continuous
   stat_summary(geom = "text", alpha = 0.5, size = 2.5, vjust = -1, hjust = 2.5,
                fun = "median", aes(label = round(after_stat(x), 1))) +
   scale_x_continuous(breaks = seq(25, 55, by = 5)) + 
   xlab("hand wing index") + 
   ylab("species")
+
+# the distiller command doesn't work well here as it melds only 7 discrete colors 
+# from an existing palette, like viridis or brewer
+# see documentation here https://cran.r-project.org/web/packages/ggplot2/ggplot2.pdf
+
 
 
 #### subset full data set for beak data
@@ -122,7 +128,7 @@ p2 <- ggplot(morph_CMs, aes(x = MEASURE, y = fct_reorder(CODE,MEASURE), fill = a
                   axis.title.x = element_text(size = 8),
                   axis.text.y = element_text(size = 7),
                   axis.title.y = element_text(size = 0),) +
-  scale_fill_gradientn(colours = c("#9e0142", "#d53e4f",  "#fdae61", "#fee08b", "#e6f598", "#abdda4", "#66c2a5", "#3288bd", "#5e4fa2")) +
+  scale_fill_gradientn(colours = c("#9e0142", "#d53e4f",  "#fdae61", "#fee08b", "#e6f598", "#abdda4", "#66c2a5", "#3288bd", "#313695")) +
   #  scale_fill_distiller(palette = "Spectral", direction = 1) + # continuous 7 color Brewer
   geom_density_ridges_gradient(scale = 2.5, alpha = 0.6, size = 0.2, rel_min_height = 0.01, bandwidth = 0.4) +
   stat_summary(geom = "text", alpha = 0.5, size = 2.5, vjust = -1, hjust = 2.5,
@@ -191,19 +197,44 @@ p4 <- ggplot(massvol, aes(x = MASS_g, y = BRAIN_ml)) +
   geom_line(alpha = 0.25, size = 3, color = '#000000',
             stat = "smooth", method = 'nls', formula = 'y~a*x^b', # power fit using non-linear squares regression
               method.args = list(start= c(a = 1,b=1)), se = FALSE) + 
-  geom_point(shape =19, size = 3.2, alpha = 0.7, 
+  geom_point(shape =19, size = 3.2, alpha = 0.8, 
              aes(color = MASS_g)) +
   geom_point(shape = 1,size = 3.2, colour = "black", stroke = 0.25,) +
-  scale_colour_gradientn(colours = c("#9e0142", "#d53e4f",  "#fdae61", "#fee08b", "#e6f598", "#abdda4", "#66c2a5", "#3288bd", "#5e4fa2")) +
+  scale_colour_gradientn(colours = c("#9e0142", "#d53e4f",  "#fdae61", "#fee08b", "#e6f598", "#abdda4", "#66c2a5", "#3288bd", "#313695")) +
   scale_y_continuous(breaks = seq(0, 25, by = 5)) +
   scale_x_continuous(breaks = seq(0, 1400, by = 200)) +
   xlab("body mass (g)") + 
   ylab("brain volume (ml)")
 
 
+# try this plot a different way, ranking by CODE class
+# but means have to force expand the palette
+
+p5 <- ggplot(massvol, aes(x = MASS_g, y = BRAIN_ml)) + 
+  themeKV + theme(axis.text.x = element_text(size = 7),
+                  axis.title.x = element_text(size = 8),
+                  axis.text.y = element_text(size = 7),
+                  axis.title.y = element_text(size = 8),
+                  legend.key.height = unit(0.3, 'cm'), # shrink the native height of legend
+                  legend.text = element_text(size=6.5)) + # reduce font size on legend
+  geom_line(alpha = 0.25, size = 3, color = '#000000',
+            stat = "smooth", method = 'nls', formula = 'y~a*x^b', # power fit using non-linear squares regression
+            method.args = list(start= c(a = 1,b=1)), se = FALSE) + 
+  geom_point(shape = 16, size = 3.2, alpha = 0.8, 
+             aes(color = fct_reorder(CODE,MASS_g))) + # color by CODE, sorted big to small by MASS_g
+  scale_color_manual(values = Spectral13) +
+  geom_point(shape = 1,size = 3.2, colour = "black", stroke = 0.25,) +
+  scale_y_continuous(breaks = seq(0, 25, by = 5)) +
+  scale_x_continuous(breaks = seq(0, 1400, by = 200)) +
+  xlab("body mass (g)") + 
+  ylab("brain volume (ml)") +
+  guides(color = guide_legend(reverse=TRUE, # reverse legend sort order to match data sort
+                              override.aes = list(size=2.6))) # reduce point size in legend
+  
+
 layout <- "
-AAAABBBBCCCCX"
-p1 + p2 + p4 + 
+ABC"
+p1 + p2 + p5 + 
   plot_layout(design = layout) +
   plot_annotation(tag_levels = 'a') # add panel labels a, b, c... etc
 
