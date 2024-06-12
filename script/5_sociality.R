2100b323e316380773814ed4e840ab31c5c2de89
vanhoutan <kyle.vanhoutan@gmail.com>
2024-06-12 13:41:56 -0400

    add viz
    revised sociality summary outputs

--- script/5_sociality.R
+++ script/5_sociality.R
@@ -35,8 +35,12 @@ themeKV <- theme_few()+
 
 #### read in beak and wing morph data
 #setwd("/Users/kylevanhoutan/colpa_flock/")
-df <- read.csv('data/sociality.csv')
-sociality <- gather(df, key="OBSERV", value="ESTIMATE", 2:9) # gather wide data to make tall db
+# df <- read.csv('data/sociality.csv')
+df <- read.csv('data/sociality2.csv')
+# sociality <- gather(df, key="OBSERV", value="ESTIMATE", 2:9) # gather wide data to make tall db
+sociality <- df %>% gather(key="OBSERV", value="ESTIMATE", 2:9) 
+sociality$OBSERV <- str_replace(sociality$OBSERV,"X", "")  
+  
 
 # make 13 color Brewer palette
 getPalette = colorRampPalette(brewer.pal(11, "Spectral")) # interpolate colors to fit the 13 parrot species
@@ -54,3 +58,42 @@ ggplot(sociality, aes(fill=fct_reorder(SPECIES,ESTIMATE, .fun=median), x=ESTIMAT
   xlab("social index value") +
   scale_x_continuous(breaks = seq(0, 100, by = 10))
 
+
+ggplot(sociality, aes(x=OBSERV, y=ESTIMATE, group = SPECIES)) +
+  themeKV + theme(axis.text.x = element_text(size = 9),
+                              axis.title.x = element_text(size = 10),
+                              axis.text.y = element_text(size = 9),
+                              axis.title.y = element_text(size = 10),
+                              legend.key.height = unit(0.4, 'cm'), # shrink the native height of legend
+                              legend.text = element_text(size=7.5)) + # reduce font size on legend
+  geom_line(linewidth = 0.25, alpha = 0.9) + 
+  geom_line(aes(color=fct_reorder(SPECIES,ESTIMATE, .fun=max)),linewidth = 2, alpha = 0.5) + 
+  geom_point(shape = 21, size = 2, stroke = 2, alpha = 0.5,
+             aes(color=fct_reorder(SPECIES,ESTIMATE, .fun=max)))+
+  geom_point(shape = 21, size = 3.2, stroke = 0.25, alpha = 0.9)+
+  scale_color_manual(values = Spectral13) + # bring in manual palette
+  ylab("cumul. social index") +
+  xlab("index component") +
+  guides(color = guide_legend(reverse=TRUE, # reverse legend sort order to match data sort
+                              override.aes = list(size=2))) # reduce point size in legend
+
+
+ggplot(sociality, aes(x=OBSERV, y=ESTIMATE, group = SPECIES)) +
+  themeKV + theme(axis.text.x = element_text(size = 9),
+                  axis.title.x = element_text(size = 10),
+                  axis.text.y = element_text(size = 9),
+                  axis.title.y = element_text(size = 10),
+                  legend.key.height = unit(0.4, 'cm'), # shrink the native height of legend
+                  legend.text = element_text(size=7.5)) + # reduce font size on legend
+  #geom_line(linewidth = 0.25, alpha = 0.9) + 
+  geom_point(shape = 21, size = 2, stroke = 1.2,
+             aes(color=fct_reorder(SPECIES,ESTIMATE, .fun=max), alpha=0.9)
+             )+
+  geom_line(aes(color=fct_reorder(SPECIES,ESTIMATE, .fun=max)),linewidth = 3, alpha = 0.7) + 
+  #geom_point(shape = 21, size = 3.2, stroke = 0.25, alpha = 0.9)+
+  scale_color_manual(values = Spectral13) + # bring in manual palette
+#  scale_fill_manual(values = Spectral13) + # bring in manual palette
+  ylab("cumul. social index") +
+  xlab("index component") +
+  guides(color = guide_legend(reverse=TRUE, # reverse legend sort order to match data sort
+                              override.aes = list(size=2))) # reduce point size in legend
